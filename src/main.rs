0ed230db91e81854fca3097d0ebc6e31b5d9ca2b
Cherry <cherry@sparklet.org>
2024-09-01 01:34:38 -0400

    clap boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    decode: bool,
+
+    data: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    match args.decode {
+        true => {
+            eprintln!("Decoding...");
+            decode(&args.data);
+        }
+
+        false => {
+            eprintln!("Encoding...");
+            encode(&args.data);
+        }
+    }
+}
+
+fn encode(data: &str) {
+    todo!()
+}
+
+fn decode(data: &str) {
+    todo!()
 }
