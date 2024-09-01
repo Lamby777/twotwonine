1f0671198502e9aef9481adc4965bf4259393989
Cherry <cherry@sparklet.org>
2024-09-01 01:50:05 -0400

    pre `color_space`

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use colors_transform::{Color, Rgb};
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -25,10 +26,37 @@ fn main() {
     }
 }
 
-fn encode(data: &str) {
+fn tuple3_to_str(tuple: (f32, f32, f32)) -> String {
+    format!("{} {} {}", tuple.0, tuple.1, tuple.2)
+}
+
+fn encode(data: &str) -> String {
+    let ascii = data.chars().map(|c| c as u8).collect::<Vec<_>>();
+    let rgbs = ascii
+        .chunks(3)
+        .map(|chunk| {
+            let r = chunk.get(0).unwrap_or(&32);
+            let g = chunk.get(1).unwrap_or(&32);
+            let b = chunk.get(2).unwrap_or(&32);
+            Rgb::from(*r as f32, *g as f32, *b as f32)
+        })
+        .collect::<Vec<_>>();
+
+    rgbs.iter()
+        .map(|rgb| tuple3_to_str(rgb.to_hsl().as_tuple()))
+        .collect()
+}
+
+fn decode(data: &str) -> String {
     todo!()
 }
 
-fn decode(data: &str) {
-    todo!()
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn encode_ily() {
+        assert_eq!(encode("ILY"), "229 18 35")
+    }
 }
